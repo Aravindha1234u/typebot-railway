5941ef3a31af0b0fabc668f3f108113fc709e1af
Aravindha1234u <aravindha1234u@gmail.com>
2023-08-25 16:55:08 -0400

    Added entrypoint

---
+++ viewer/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# ENVSH_ENV=./apps/viewer/.env.production ENVSH_OUTPUT=./apps/viewer/public/__env.js bash inject-runtime-env.sh
+
+./node_modules/.bin/prisma generate --schema=packages/prisma/postgresql/schema.prisma;
+
+node apps/viewer/server.js;
